3cd3e8dcf097d9d1390cf8d5f2179c5cb1acad0c
Otabek Tuychiev <otawflash@gmail.com>
2025-05-02 19:59:09 +0900

    Initial Commit

---
+++ Barberist/BarberistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarberistApp.swift
+//  Barberist
+//
+//  Created by Otabek Tuychiev on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BarberistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
